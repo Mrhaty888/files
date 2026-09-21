273c905bbf3d40f6b7fd0258fd9459eaee69dbad
Marco Sancho Santana dos santos <marcaoeele@gmail.com>
2026-09-21 16:11:31 -0300

    Add files via upload

--- exercicio struct.c
+++ exercicio struct.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+#include <string.h>
 
 struct alunos {
     char aluno[50];
@@ -9,79 +9,109 @@ struct alunos {
     float media;
 };
 
-int main() {
-    struct alunos aluno1, aluno2, aluno3;
+void main(){
+    struct alunos aluno1;
+    struct alunos aluno2;
+    struct alunos aluno3;
     char nomes[3][50];
     float m1, m2;
+    int ra1, ra2;
     int posicao = 0;
     int i;
 
     setlocale(LC_ALL, "Portuguese");
     setlocale(LC_NUMERIC, "C");
 
+    fflush(stdin);
     printf("Digite seu nome: ");
-    scanf(" %[^\n]s", aluno1.aluno);
+    scanf("%s", aluno1.aluno);
     printf("Digite seu RA: ");
     scanf("%d", &aluno1.RA);
+    fflush(stdin);
     printf("Digite sua cidade: ");
-    scanf(" %[^\n]s", aluno1.cidade);
-    printf("Digite sua média (use ponto, ex: 7.5): ");
+    scanf("%s", aluno1.cidade);
+    printf("Digite sua média: ");
     scanf("%f", &aluno1.media);
 
-    if (_stricmp(aluno1.cidade, "marilia") == 0 || strcmp(aluno1.cidade, "marília") == 0 || strcmp(aluno1.cidade, "maríLIA") == 0 || strcmp(aluno1.cidade, "MARÍLIA") == 0) {
-        strcpy(nomes[posicao], aluno1.aluno);
+    _strlwr(aluno1.cidade);
+    if (strcmp(aluno1.cidade, "marilia") == 0) {
+        sprintf(nomes[posicao], "%s", aluno1.aluno);
         posicao++;
     }
 
+    fflush(stdin);
     printf("Digite seu nome: ");
-    scanf(" %[^\n]s", aluno2.aluno);
+    scanf("%s", aluno2.aluno);
     printf("Digite seu RA: ");
     scanf("%d", &aluno2.RA);
+    fflush(stdin);
     printf("Digite sua cidade: ");
-    scanf(" %[^\n]s", aluno2.cidade);
-    printf("Digite sua média (use ponto, ex: 7.5): ");
+    scanf("%s", aluno2.cidade);
+    printf("Digite sua média: ");
     scanf("%f", &aluno2.media);
 
-    if (_stricmp(aluno2.cidade, "marilia") == 0 || strcmp(aluno2.cidade, "marília") == 0 || strcmp(aluno2.cidade, "maríLIA") == 0 || strcmp(aluno2.cidade, "MARÍLIA") == 0) {
-        strcpy(nomes[posicao], aluno2.aluno);
+    _strlwr(aluno2.cidade);
+    if (strcmp(aluno2.cidade, "marilia") == 0) {
+        sprintf(nomes[posicao], "%s", aluno2.aluno);
         posicao++;
     }
 
+    fflush(stdin);
     printf("Digite seu nome: ");
-    scanf(" %[^\n]s", aluno3.aluno);
+    scanf("%s", aluno3.aluno);
     printf("Digite seu RA: ");
     scanf("%d", &aluno3.RA);
+    fflush(stdin);
     printf("Digite sua cidade: ");
-    scanf(" %[^\n]s", aluno3.cidade);
-    printf("Digite sua média (use ponto, ex: 7.5): ");
+    scanf("%s", aluno3.cidade);
+    printf("Digite sua média: ");
     scanf("%f", &aluno3.media);
 
-    if (_stricmp(aluno3.cidade, "marilia") == 0 || strcmp(aluno3.cidade, "marília") == 0 || strcmp(aluno3.cidade, "maríLIA") == 0 || strcmp(aluno3.cidade, "MARÍLIA") == 0) {
-        strcpy(nomes[posicao], aluno3.aluno);
+    _strlwr(aluno3.cidade);
+    if (strcmp(aluno3.cidade, "marilia") == 0) {
+        sprintf(nomes[posicao], "%s", aluno3.aluno);
         posicao++;
     }
 
     if (aluno1.media >= aluno2.media && aluno1.media >= aluno3.media) {
         m1 = aluno1.media;
-        if (aluno2.media >= aluno3.media) m2 = aluno2.media;
-        else m2 = aluno3.media;
+        ra1 = aluno1.RA;
+        if (aluno2.media >= aluno3.media) {
+            m2 = aluno2.media;
+            ra2 = aluno2.RA;
+        } else {
+            m2 = aluno3.media;
+            ra2 = aluno3.RA;
+        }
     }
     else if (aluno2.media >= aluno1.media && aluno2.media >= aluno3.media) {
         m1 = aluno2.media;
-        if (aluno1.media >= aluno3.media) m2 = aluno1.media;
-        else m2 = aluno3.media;
+        ra1 = aluno2.RA;
+        if (aluno1.media >= aluno3.media) {
+            m2 = aluno1.media;
+            ra2 = aluno1.RA;
+        } else {
+            m2 = aluno3.media;
+            ra2 = aluno3.RA;
+        }
     }
     else {
         m1 = aluno3.media;
-        if (aluno1.media >= aluno2.media) m2 = aluno1.media;
-        else m2 = aluno2.media;
+        ra1 = aluno3.RA;
+        if (aluno1.media >= aluno2.media) {
+            m2 = aluno1.media;
+            ra2 = aluno1.RA;
+        } else {
+            m2 = aluno2.media;
+            ra2 = aluno2.RA;
+        }
     }
 
-    printf("\nAlunos que moram em Marília: ");
+    printf("Alunos que moram em marília: ");
     for (i = 0; i < posicao; i++) {
         printf("%s ", nomes[i]);
     }
-    printf("\nAlunos com as maiores médias: %.1f %.1f\n", m1, m2);
-
-    return 0;
+    printf("\nAlunos com as maiores médias: ");
+    printf("%d , ", ra1);
+    printf("%d .", ra2);
 }
